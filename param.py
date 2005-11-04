8bb5c72d4418ba1de1883221357872e3f351c59d
Norbert Nemec <nen10015@pc53687.physik.uni-regensburg.de>
2005-11-04 14:18:11 +0100

    new param handling

--- param.py
+++ param.py
@@ -15,48 +15,27 @@ class DataDummy:
         del self._v_attrnamesuser[name]
 
 class Param:
-    def setdefaults(self):
-        data = self.data
-
-        for name in data._v_attrnamesuser:
-            delattr(data,name)
-
-        data.LOPEZ_SANCHO_ETA = 1e-5*eV
-        data.LOPEZ_SANCHO_EPSILON = 1e-4*eV
-
-        data.GRAPHENE_CC_DISTANCE = 1.4226*angstrom
-        data.GRAPHITE_INTERLAYER_DISTANCE = 3.44*angstrom
-        data.GRAPHENE_1STNN_HOPPING = 2.66*eV
-
-        data.LATTICE_CONSTANT = data.GRAPHENE_CC_DISTANCE
-        data.HOPPING_CONSTANT = data.GRAPHENE_1STNN_HOPPING
-
-#        data.LEAD_TYPE = "wideband"
-#        data.LEAD_TYPE = "coating_wideband"
-#        data.WIDEBAND_ENERGY = 1.0*eV
-        data.LEAD_TYPE = "lopez_sancho"
-
-#        data.DO_CALC_CHANNELS = True
-#        data.NO_DISORDER_IN_CONTACTS = True
-        data.BFIELD_IN_LEADS = True
-        data.BFIELD_DIRECTION = 'perp'
-#        data.BFIELD_DIRECTION = 'par'
-
-        data.DISORDER_TYPE = "diagonal"
-#        data.DISORDER_TYPE = "hopping"
-
-        data.TRIOZON_BETA = data.GRAPHENE_1STNN_HOPPING / 8
-        data.TRIOZON_A = 0.334 * nm
-        data.TRIOZON_DELTA = 0.045 * nm
-        data.TRIOZON_CUTOFF = data.TRIOZON_A+5*data.TRIOZON_DELTA
-        data.TRIOZON_Z_CUTOFF = (data.TRIOZON_CUTOFF**2 - (0.95*data.GRAPHITE_INTERLAYER_DISTANCE)**2)**0.5
-
     def reset(self):
         self.__dict__['temp'] = {}
         self.__dict__['data'] = DataDummy()
 
     def __init__(self):
+	self.__dict__['defaults'] = {}
         self.reset()
+
+    def setdefaults(self):
+        data = self.data
+
+        for name in data._v_attrnamesuser:
+            delattr(data,name)
+	    
+	for name,value in self.defaults.iteritems():
+	    setattr(data,name,value)
+
+    def createdefault(self,name,value):
+	self.defaults[name] = value
+	if not hasattr(data,name):
+	    setattr(data,name,value)
 
     def activate(self,data,copy=False):
         if isinstance(data,pytables.Group):
@@ -95,3 +74,42 @@ class Param:
         return name in self.temp or hasattr(self.data,name)
 
 param = Param()
+
+
+
+
+    def setdefaults(self):
+        data = self.data
+
+        for name in data._v_attrnamesuser:
+            delattr(data,name)
+
+param.createdefault("LOPEZ_SANCHO_ETA", 1e-5*eV)
+param.createdefault("LOPEZ_SANCHO_EPSILON", 1e-4*eV)
+
+param.createdefault("GRAPHENE_CC_DISTANCE", 1.4226*angstrom)
+param.createdefault("GRAPHITE_INTERLAYER_DISTANCE", 3.44*angstrom)
+param.createdefault("GRAPHENE_1STNN_HOPPING", 2.66*eV)
+
+param.createdefault("LATTICE_CONSTANT", data.GRAPHENE_CC_DISTANCE)
+param.createdefault("HOPPING_CONSTANT", data.GRAPHENE_1STNN_HOPPING)
+
+#        data.LEAD_TYPE", "wideband"
+#        data.LEAD_TYPE", "coating_wideband"
+#        data.WIDEBAND_ENERGY", 1.0*eV
+param.createdefault("LEAD_TYPE", "lopez_sancho")
+
+#        data.DO_CALC_CHANNELS", True
+#        data.NO_DISORDER_IN_CONTACTS", True
+param.createdefault("BFIELD_IN_LEADS", True)
+param.createdefault("BFIELD_DIRECTION", 'perp')
+#        data.BFIELD_DIRECTION", 'par'
+
+param.createdefault("DISORDER_TYPE", "diagonal")
+#        data.DISORDER_TYPE", "hopping"
+
+param.createdefault("TRIOZON_BETA", param.GRAPHENE_1STNN_HOPPING / 8)
+param.createdefault("TRIOZON_A", 0.334 * nm)
+param.createdefault("TRIOZON_DELTA", 0.045 * nm)
+param.createdefault("TRIOZON_CUTOFF", param.TRIOZON_A+5*param.TRIOZON_DELTA)
+param.createdefault("TRIOZON_Z_CUTOFF", (param.TRIOZON_CUTOFF**2 - (0.95*param.GRAPHITE_INTERLAYER_DISTANCE)**2)**0.5)
