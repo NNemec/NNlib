21550d36a05cf94b517ddd8a61f1ead1f539c052
Norbert Nemec <nen10015@pc55842.physik.uni-regensburg.de>
2006-08-09 14:56:23 +0200

    cope with hoppings longer than unit cell

--- chain.py
+++ chain.py
@@ -18,11 +18,14 @@ param.createdefault("TRIOZON_Z_CUTOFF", (param.TRIOZON_CUTOFF**2 - (0.95*param.G
 
 class chain:
     def __init__(self,H_int_B0,H_hop_B0,xyz_chain=None,do_cache=True):
-        assert type(H_int_B0) is type(Matrix(()))
-        assert type(H_hop_B0) is type(Matrix(()))
+        assert type(H_int_B0) is matrix
         self.N_atoms = H_int_B0.shape[0]
-        assert(H_int_B0.shape == (self.N_atoms,self.N_atoms))
-        assert(H_hop_B0.shape == (self.N_atoms,self.N_atoms))
+        assert H_int_B0.shape == (self.N_atoms,self.N_atoms)
+        if type(H_hop_B0) is matrix:
+            H_hop_B0 = [H_hop_B0]
+        for hhop in H_hop_B0:
+            assert type(hhop) is matrix
+            assert hhop.shape == (self.N_atoms,self.N_atoms)
         self.H_int = H_int_B0
         self.H_hop = H_hop_B0
         self.H_int_B0 = H_int_B0
@@ -30,9 +33,9 @@ class chain:
 
         if xyz_chain is not None:
             assert isinstance(xyz_chain,xyz.chain)
+            assert len(xyz_chain.atoms) == self.N_atoms
             self.xyz = xyz_chain
-            self.xyz_shifted = xyz_chain.shift(xyz_chain.period)
-            assert self.N_atoms == len(self.xyz.atoms)
+            self.xyz_shifted = [ xyz_chain.shift(xyz_chain.period * (i+1)) for i in range(len(H_hop_B0)) ]
             self.bfield = array((0,0,0))
 
         self.energy = None
@@ -51,22 +54,22 @@ class chain:
                 self.H_hop = self.H_hop_B0
             else:
                 self.H_int = bf.calc_H_int(self.bfield,self.H_int_B0,self.xyz)
-                self.H_hop = bf.calc_H_hop(self.bfield,self.H_hop_B0,self.xyz,self.xyz_shifted)
-	    if hasattr(self,'cache'):
-        	self.cache = {}
-	    (self._G_bulk,self._Gs_L,self._Gs_R) = (None,None,None)
+                self.H_hop = [ bf.calc_H_hop(self.bfield,self.H_hop_B0[i],self.xyz,self.xyz_shifted[i]) for i in range(len(self.H_hop_B0)) ]
+            if hasattr(self,'cache'):
+                self.cache = {}
+            (self._G_bulk,self._Gs_L,self._Gs_R) = (None,None,None)
 
     def set_energy(self,energy):
         if energy is None:
             assert self.energy is not None
         else:
-	    if energy != self.energy:
-		(self._G_bulk,self._Gs_L,self._Gs_R) = (None,None,None) 
+            if energy != self.energy:
+                (self._G_bulk,self._Gs_L,self._Gs_R) = (None,None,None)
             self.energy = energy
-	if self._G_bulk is None:
-	    if hasattr(self,'cache') and self.energy in self.cache:
-        	(self._G_bulk,self._Gs_L,self._Gs_R) = self.cache[self.energy]
-	    else:
+        if self._G_bulk is None:
+            if hasattr(self,'cache') and self.energy in self.cache:
+                (self._G_bulk,self._Gs_L,self._Gs_R) = self.cache[self.energy]
+            else:
                 self._do_calc_lopez_sancho()
                 if hasattr(self,'cache'):
                     self.cache[self.energy] = (self._G_bulk,self._Gs_L,self._Gs_R)
@@ -77,8 +80,9 @@ class chain:
 
         # alpha = energy*S_hop - chain.H_hop;
         # beta = energy*chain.S_hop' - chain.H_hop';
-        alpha = - self.H_hop
-        beta = - adj(self.H_hop)
+        assert len(self.H_hop) == 1
+        alpha = - self.H_hop[0]
+        beta = - adj(self.H_hop[0])
         epsilon = E - self.H_int
         epsilon_L = epsilon
         epsilon_R = epsilon
@@ -116,7 +120,10 @@ class chain:
         return self._G_bulk
 
     def H_eff(self,k):
-        return self.H_int + exp(1j*k)*self.H_hop + exp(-1j*k)*adj(self.H_hop)
+        res = self.H_int + 0.0
+        for i in range(len(self.H_hop)):
+            res += exp(1j*k*(i+1))*self.H_hop[i] + exp(-1j*k*(i+1))*adj(self.H_hop[i])
+        return res
 
     def band_energies(self,k):
         return array(sorted(list(real(eigvalsh(self.H_eff(k))))))
@@ -149,13 +156,14 @@ class chain:
         if hasattr(self,'xyz'):
             xyz = self.xyz.multiply(N)
         H_int = Matrix(zeros((N*A,N*A),'D'))
+        assert len(H_hop_B0) == 1
         for n in range(N):
             H_int[n*A:(n+1)*A,n*A:(n+1)*A] = self.H_int_B0
         for n in range(1,N):
-            H_int[(n-1)*A:n*A,n*A:(n+1)*A] = self.H_hop_B0
-            H_int[n*A:(n+1)*A,(n-1)*A:n*A] = adj(self.H_hop_B0)
-        H_hop = Matrix(zeros((N*A,N*A),'D'))
-        H_hop[(N-1)*A:N*A,0:A] = self.H_hop_B0
+            H_int[(n-1)*A:n*A,n*A:(n+1)*A] = self.H_hop_B0[0]
+            H_int[n*A:(n+1)*A,(n-1)*A:n*A] = adj(self.H_hop_B0[0])
+        H_hop = [Matrix(zeros((N*A,N*A),'D'))]
+        H_hop[0][(N-1)*A:N*A,0:A] = self.H_hop_B0[0]
         return chain(H_int,H_hop,xyz)
 
 def square_ladder(N,gamma,do_cache=True):
@@ -200,34 +208,63 @@ def tight_binding_dwcnt_triozon(xyz_tube_A,xyz_tube_B,do_cache=True):
     # based on the parametrization described in
     # doi:10.1103/PhysRevB.64.121401
 
-    def interwall_hopping(pos_a,pos_b):
-        d = norm(pos_b-pos_a);
-        cos_theta = vdot(pos_a[:2],pos_b[:2])/(norm(pos_a[:2])*norm(pos_b[:2]));
-        return -param.TRIOZON_BETA * cos_theta * exp((param.TRIOZON_A - d)/param.TRIOZON_DELTA);
+    CC_DIST = param.GRAPHENE_CC_DISTANCE
+    NN_HOP = param.GRAPHENE_1STNN_HOPPING
 
-    xyz_chain = xyz.merge(xyz_tube_A,xyz_tube_B)
+    TRIO_CUTOFF = param.TRIOZON_CUTOFF
+    Z_CUTOFF = param.TRIOZON_Z_CUTOFF
+    BETA = param.TRIOZON_BETA
+    A = param.TRIOZON_A
+    DELTA = param.TRIOZON_DELTA
 
-    (H_int_A,H_hop_A,N_A) = _tight_binding_1stNN_graphene_H(xyz_tube_A)
-    (H_int_B,H_hop_B,N_B) = _tight_binding_1stNN_graphene_H(xyz_tube_B)
-    H_int = Matrix(zeros((N_A+N_B,N_A+N_B),'D'))
-    H_int[:N_A,:N_A] = H_int_A
-    H_int[N_A:,N_A:] = H_int_B
-    H_hop = Matrix(zeros((N_A+N_B,N_A+N_B),'D'))
-    H_hop[:N_A,:N_A] = H_hop_A
-    H_hop[N_A:,N_A:] = H_hop_B
+    def hopping(pos_a,pos_b):
+        if abs(pos_a[2] - pos_b[2]) > Z_CUTOFF:
+            return 0.0
+        elif abs(norm(pos_a[:2])-norm(pos_b[:2])) < CC_DIST*0.1:
+            if norm(pos_a - pos_b) < CC_DIST*1.1:
+                return -NN_HOP
+        else:
+            d = norm(pos_b-pos_a);
+            if d < TRIO_CUTOFF:
+                cos_theta = vdot(pos_a[:2],pos_b[:2])/(norm(pos_a[:2])*norm(pos_b[:2]));
+                return -BETA * cos_theta * exp((A - d)/DELTA);
+        return 0.0
 
-    for b in range(N_B):
-        pos_b = xyz_tube_B.atoms[b].pos
-        pos_b_hop = pos_b + xyz_chain.period
-        for a in range(N_A):
-            pos_a = xyz_tube_A.atoms[a].pos
-            pos_a_hop = pos_a + xyz_chain.period
-            H_int[a,N_A+b] = interwall_hopping(pos_a,pos_b)
-            H_int[N_A+b,a] = H_int[a,N_A+b]
-            H_hop[a,N_A+b] = interwall_hopping(pos_a,pos_b_hop)
-            H_hop[N_A+b,a] = interwall_hopping(pos_b,pos_a_hop)
+    x = xyz.merge(xyz_tube_A,xyz_tube_B)
+    at = x.atoms
+    period = x.period
 
-    return chain(H_int,H_hop,xyz_chain,do_cache)
+    Natoms = len(x.atoms)
+    H_int = Matrix(zeros((Natoms,Natoms),'D'))
+
+    for i in range(Natoms):
+        for j in range(i+1,Natoms):
+            hop = hopping(at[i].pos,at[j].pos)
+            if hop != 0.0:
+                H_int[i,j] = hop
+                H_int[j,i] = conj(hop)
+
+    H_hop = []
+    for n in range(20):
+#    for n in range(1+int(Z_CUTOFF/period[2])):
+#    for n in [0]:
+        x_shifted = x.shift(period*(n+1))
+        at_sh = x_shifted.atoms
+
+        h_hop = Matrix(zeros((Natoms,Natoms),'D'))
+        nonzero = False
+        for i in range(Natoms):
+            for j in range(Natoms):
+                hop = hopping(at[i].pos,at_sh[j].pos)
+                if hop != 0.0:
+                    nonzero = True
+                    h_hop[i,j] = hop
+        if nonzero:
+            H_hop.append(h_hop)
+        else:
+            break
+
+    return chain(H_int,H_hop,x,do_cache=do_cache)
 
 
 if __name__ == "__main__":
@@ -237,4 +274,3 @@ if __name__ == "__main__":
 
     a = ch.Gs_L(energy=0.5)
     b = ch.Gs_L(energy=1.0)
-    
